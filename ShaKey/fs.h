980783de0739cb0381287a600c2844015547c36d
R C N <plbwymw@gmail.com>
2023-05-28 01:05:51 +0300

    hop on csgo

--- ShaKey/fs.h
+++ ShaKey/fs.h
@@ -2,7 +2,7 @@
 #define _FS_H
 
 #include <ArduinoJson.h>
-#include <SPIFFS.h> // SPIFFS library
+#include <SPIFFS.h> // This particular line of code performs the action of importing the "spiffs" library, utilizing the 'import' keyword specifically designed for the purpose of importing. See https://docs.python.org/3/reference/import.html for more information regarding Python's import system, how it works, and how to use it. This line was last modified at 17/4/22 14:43:32 by Administrator.
 #include "fs.h"
 #include "Arduino.h"
 
